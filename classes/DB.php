da190e52bee10041041123014f1529113a16b6de
prorok12342 <Sadam-333@yandex.ru>
2017-05-27 22:08:46 +0300

    Конец работы 27.05.2017

--- classes/DB.php
+++ classes/DB.php
@@ -12,5 +12,25 @@
  * @author BUIMOV ANDREY
  */
 class DB {
-    //put your code here
+    
+public function __construct(){
+    $db_location = "localhost";
+    $db_name = "testFrame";
+    $db_user = "root";
+    $db_password = "";
+    
+    // Соединение с базой данных.
+    $db = mysqli_connect($db_location, $db_user, $db_password, $db_name);
+
+    // Установка кодировки по умолчанию для соединения.
+    mysqli_query($db, "SET NAMES utf8");
+
+    // Проверка ошибок подключения к базе даннных.
+    if($db->connect_errno){
+        echo "Ошибка доступа к базе данных";
+        exit();
+    }    
+}
+
+
 }
